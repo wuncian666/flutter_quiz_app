71573e2f4be1a712fd2d0eca89c377d4e856334a
wuncian <wuncian666@gmail.com>
2024-01-06 21:31:06 +0800

    Initial commit

---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,52 @@
+import 'package:adv_basics/questions_summary/question_identifier.dart';
+import 'package:flutter/material.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(this.itemData, {super.key});
+
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        QuestionIdentifier(
+          isCorrectAnswer: isCorrectAnswer,
+          questionIndex: itemData['question_index'] as int,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              itemData['question'] as String,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(itemData['user_answer'] as String,
+                style: const TextStyle(
+                  color: Colors.black54,
+                )),
+            Text(itemData['correct_answer'] as String,
+                style: TextStyle(
+                  color: isCorrectAnswer ? Colors.green : Colors.red,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
+        ))
+      ],
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,76 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void resatrtQuiz() {
+    setState(() {
+      activeScreen = 'questions-screen';
+      selectedAnswers = [];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: resatrtQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue,
+                Colors.green,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
